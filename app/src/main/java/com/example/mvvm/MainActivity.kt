534159b41f90d48dcc7fdb9983d58f8af4d1e446
Yago Militão <37947421+YagoMilitao@users.noreply.github.com>
2024-08-26 19:26:52 -0300

    First Commit

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -24,12 +24,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
+
 
         binding.buttonLogin.setOnClickListener(this)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
